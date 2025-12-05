f043442666b9705668d1192fd6eb35b56db582f4
Kevin-1224 <kapupialess@utn.edu.ec>
2025-12-04 20:33:04 -0500

    Cambio

--- Biblioteca.Api/Program.cs
+++ Biblioteca.Api/Program.cs
@@ -11,10 +11,14 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+//if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
